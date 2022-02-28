5c627ca9ad8c2924386c8c3413062649acac5e8f
Nasimul Rahman <nasimul@cynergon.net>
2022-02-28 16:21:32 +0800

    validations veirified:

--- app/models/coach_session.rb
+++ app/models/coach_session.rb
@@ -5,3 +5,5 @@ class CoachSession < ApplicationRecord
   validates :price_per_day, numericality: true
   validates :type_of_activity, inclusion: {in: ACTIVITIES}
 end
+
+# CoachSession.create(session_name:"Bench Press Training", type_of_activity:"Weight-Lifting", price_per_day: 30, user_id: 4)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
 
   validates :user_name, :email_address, :phone_number, presence: true
   validates :user_name, :email_address, :phone_number, uniqueness: true
-  validates :user_name, length: { minimum: 10 }
+  validates :user_name, length: { minimum: 4 }
+  validates :user_name, length: { maximum: 50 }
 end
+
+# User.create(user_name:"Nasimul", email_address:"nasimulrahman@gmail.com", phone_number:"0123456789").valid?
