33522b151270c28a64ea20bc78c8dcd04698d5b5
Nasimul Rahman <nasimul@cynergon.net>
2022-02-28 15:39:09 +0800

    Validations and associations added to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   belongs_to :coach_session
+  validates :date_start, :date_end, numericality: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :coach_session
+  has_many :bookings
+
+  validates :user_name, :email_address, :phone_number, presence: true
+  validates :user_name, :email_address, :phone_number, uniqueness: true
+  validates :user_name, length: { minimum: 10 }
 end
