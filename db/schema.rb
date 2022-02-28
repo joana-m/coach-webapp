04f6e5f3dca09a2e83a9d788c50832beaeee2e74
Nasimul Rahman <nasimul@cynergon.net>
2022-02-28 17:01:33 +0800

    new user created

--- db/schema.rb
+++ db/schema.rb
@@ -39,12 +39,15 @@ ActiveRecord::Schema.define(version: 2022_02_28_065531) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name"
-    t.string "email_address"
-    t.string "phone_number"
-    t.string "avatar"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "coach_sessions"
