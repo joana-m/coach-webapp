7c2d384fe0a54e7821219fc4489dc971279f1d1b
Nasimul Rahman <nasimul@cynergon.net>
2022-03-01 15:13:13 +0800

    completed registration

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :coach_session, dependent: :destroy
 
   validates :total_amount, :date_start, :date_end, presence: true
+  has_one_attached :avatar
 end
